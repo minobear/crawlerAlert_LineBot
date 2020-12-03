b623aa9259cc40c4539ca0447e873e1b3796dc23
weimino <soupmill@gmail.com>
2020-12-03 14:29:01 +0800

    More Pro360 information

--- crawler_handler/pro360_crawler.py
+++ crawler_handler/pro360_crawler.py
@@ -18,7 +18,7 @@ def get_new_pro360_info():
     quote_bids = res.json()["quote_bids"]
 
     current_milli_time = (lambda: int(round(time.time() * 1000)))
-    posted_ids = db.fetchAll("SELECT id FROM pro360_list WHERE post_time >= %s", (current_milli_time() - 86400000,))
+    posted_ids = db.fetchAll("SELECT id FROM pro360_list")
 
     for i in range(len(quote_bids)):
         quote_bid = quote_bids[i]
@@ -26,6 +26,10 @@ def get_new_pro360_info():
         if id not in map(lambda ele: str(ele[0]), posted_ids):
             QuoteRequest = quote_bid["QuoteRequest"]
             title = QuoteRequest["title"]
+            manual_fee = QuoteRequest["manual_fee"]  # 聯繫費用
+            bid_count_limit = QuoteRequest["bid_count_limit"]  # 最大連繫名額
+            quote_bid_count = QuoteRequest["quote_bid_count"]  # 已連繫次數
+            bid_count_remaining = int(bid_count_limit) - int(quote_bid_count)  # 剩餘聯繫名額
             form_summary = json.loads(QuoteRequest["form_summary"])
             content = ""
             count = 0
@@ -40,6 +44,6 @@ def get_new_pro360_info():
 
             online_date = QuoteRequest["created"]
             interval_line = '=' * 25 + "\n"
-            send_broadcast_message(f"💼 Pro360有新的案件機會囉！\n\n類型: {title}\n連結: https://www.pro360.com.tw/dashboard/requests/{id}\n{interval_line}{content}\n{interval_line}{online_date}")
+            send_broadcast_message(f"💼 Pro360有新的案件機會囉！\n\n類型: {title}\n聯繫費用: ${manual_fee}\n剩餘名額: {bid_count_remaining} / {bid_count_limit}\n連結: https://www.pro360.com.tw/dashboard/requests/{id}\n{interval_line}{content}\n{interval_line}{online_date}")
             db.execute("INSERT INTO pro360_list VALUES (%s,%s,%s)", (id,current_milli_time(), form_summary))
             print(f"[{time.strftime('%H:%M:%S')}] 新的案件機會已通知並加進資料庫中: ", "[" + id + "]", title)
